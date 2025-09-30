b26da3e2035e9da60798346d8dd3c2c464370bc0
“sams7891” <samietisrodrigo@gmail.com>
2025-09-30 10:05:36 +0300

    Sākšu darbu ar programmas koda sadalīšanu metodēs

--- src/pakotne/Izvelne.java
+++ src/pakotne/Izvelne.java
@@ -1,10 +1,21 @@
 package pakotne;
 
+import javax.swing.JOptionPane;
+
 public class Izvelne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String izvele;
+		String[] darbibuSaraksts = {"Ievadīt studentus", "Ievadīt vērtēšanas kritērijus", "Aprēķināt atzīmes", "Apskatīt failu", "Apturēt"};
+		do {
+			
+			izvele = (String) JOptionPane.showInputDialog(null, "Izvēlies operāciju", "Izvēlne", JOptionPane.QUESTION_MESSAGE, null, darbibuSaraksts, darbibuSaraksts[0]);
+			
+			if(izvele == null)
+				izvele = "Apturēt";
+			
+		}while(!izvele.equals("Apturēt") );
 	}
 
 }
